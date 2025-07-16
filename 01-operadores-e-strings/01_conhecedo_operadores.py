c8290dbddd90d46f61ce7669585ba0d76fdd5bc7
jeovan <jeovanbispoo@gmail.com>
2025-07-16 20:10:25 -0300

    Adcionei a Explicação e Exercícios sobre os Operadores

---
+++ 01-operadores-e-strings/01_conhecedo_operadores.py
@@ -0,0 +1,30 @@
+#Conhecendo os Operadores Aritméticos 
+#Oque são  ? Os operadores aritméticos executam operações matemáticas, como adição, substração com operadores.
+
+# Adição
+print (1 + 1)
+>>>  2
+
+# Subtração
+print (10 - 2)
+>>> 8
+
+# Multiplicação
+print (4 * 3)
+>>> 12
+
+# Divisão
+print (12 / 3)
+>>> 4.0
+
+# Divisão Inteira
+print (12 // 2)
+>>> 6
+
+# Módulo (Resto da Divisão)
+print (10 % 3)
+>>> 1 
+
+# Exponenciação
+print (2 ** 3)
+>>> 8
