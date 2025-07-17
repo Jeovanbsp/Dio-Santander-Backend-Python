d31ab878a4a579f246baa0922e0d83a684a60332
jeovan <jeovanbispoo@gmail.com>
2025-07-16 21:14:15 -0300

    Realizado as Atividades sobre Operadores de Comparação e Adcionado a Explicação

---
+++ Operadores Aritméticos/03_conhecendo_operadores_de_comparação.py
@@ -0,0 +1,38 @@
+#Operadores de Comparação
+#O que São? São operadores utilizados para comparar dois valores
+
+saldo = 450
+saque = 200
+
+#Rertorna Valores Booleanos (True ou False)
+
+#Igualdade
+
+print (saldo == saque) #False
+
+#Diferença
+
+print (saldo != saque) #True
+
+#Maior que / maior ou igual
+
+print (saldo > saque) #True
+
+print (saldo >= saque) #True
+
+#Menor que / menor ou igual
+
+print (saldo < saque) #False
+
+print (saldo <= saque) #False
+
+#Operadores de Comparação (Exercício Prático)
+saldo = 200
+saque = 200
+
+print(saldo == saque)  # True
+print(saldo != saque)  # False
+print(saldo > saque)   # False
+print(saldo >= saque)  # True
+print(saldo < saque)   # False
+print(saldo <= saque)  # True
