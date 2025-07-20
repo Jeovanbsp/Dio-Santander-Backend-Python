02662434a28a7ecbd8e4483267376855f86184f4
jeovan <jeovanbispoo@gmail.com>
2025-07-20 12:03:13 -0300

    Realizado Atividade, Conceito e explicação sobre Operadores de Atribuição

---
+++ Operadores Aritméticos/04_operadores_de_atribuição.py
@@ -0,0 +1,81 @@
+#Seção : Operadores de Atribuição
+#Objetivo Geral : Oque são Operadores de atribuição e como utiliza-los.
+#Etapa 1 : Conhecendo os Operadores de atribuição.
+# Oque são ? São operadores utilizado para definir o valor inicial ou sobrescrever o valor de uma váriavel.
+
+#Atribuição Simples
+Saldo = 500 #(Estou atribuindo o valor 500 para a palavra Saldo)
+print (Saldo) #(Quando Leio a váriavel Saldo ele retorno o valor atribuido 500)
+# >>> 500
+
+#Atribuição com Adição
+Saldo = 500
+Saldo += 200 # (É adicionado o valor de 200 do valor atribuido anteriormente ao valor saldo que era 500 retornando o valor atual para Saldo que será 700)
+print (Saldo)
+# >>> 700
+
+#Atribuição com Subtração
+Saldo = 500
+Saldo -= 100 #(É Subtraido o valor de 100 do valor atribuido anteriormente ao valor saldo que era 500 retornando o valor atual para Saldo que será 400 )
+print(Saldo)
+# >>> 400
+
+#Atribuição com Multiplicação
+Saldo = 500
+Saldo *= 2 #(É multiplicado o valor atribuido a saldo por 2 retornando o valor atual para Saldo que será 1000)
+print(Saldo)
+# >>> 1000
+
+#Atribuição com Divisão
+Saldo = 500
+Saldo /= 5 #(É realizado a divisão do valor 500 por 5 e retornando o novo valor de Saldo para 100.0)
+print(Saldo)
+# >>> 100.0
+
+#Atribuição com Resto da Divisão
+Saldo = 500
+Saldo // 5 #(É Realizado a Divisão e Retornado o Resto dessa Divisão que é atribuido o novo valor para Saldo)
+print (Saldo)
+# >>> 100
+
+#Atribuição com Módulo
+Saldo = 500
+Saldo %= 480 #(É realizado a conta com Módulo e Retornando o novo valor para Saldo)
+print(Saldo)
+# >>> 20
+
+#Atribuição com exponenciação
+Saldo = 80
+Saldo **= 2 #(É reazliado a Exponenciação do valor de 80 por 2 e retornando o valor atualizado para Saldo de 6400)
+print(Saldo)
+# >>> 6400
+
+
+#Atividade Realizada em Aula
+
+saldo = 500
+
+print (saldo)
+
+saldo = 200
+
+print (saldo)
+
+saldo += 10
+print (saldo)
+saldo -= 5
+print (saldo)
+saldo //= 2
+print (saldo)
+saldo /= 2
+print (saldo)
+saldo *= 10
+print (saldo)
+
+
+saldo %= 4
+print (saldo)
+
+saldo **= 2
+print (saldo)
+
